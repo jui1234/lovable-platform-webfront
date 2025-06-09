50b7508ef8bf30bd151423d70deeead4dad0b11e
Jui Yezarkar <jayesh@traversetec.co>
2025-06-09 12:54:45 +0530

    fix:home page url change

--- src/components/WhyChooseUs.tsx
+++ src/components/WhyChooseUs.tsx
@@ -24,7 +24,7 @@ const WhyChooseUs = () => {
         {/* Image */}
         <div className="relative">
           <img
-            src="https://ibb.co/DHP6HybD"
+            src="https://i.ibb.co/W4Nd4w3W/20250609-1159-Healthcare-Innovation-Concept-remix-01jx9nz1nhee2841dpxy93ewtn-1.png"
             alt="Modern pharmaceutical facility"
             className="w-full h-96 object-cover rounded-2xl shadow-2xl"
           />
